535f2b2ddbb58b6084e4a37e5755543351922a7f
willilsl <32499493+willilsl@users.noreply.github.com>
2020-02-14 09:53:18 +0100

    new model & callbacks
    added early stop and checkpoints

--- load_model.py
+++ load_model.py
@@ -7,23 +7,28 @@ os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 from keras.models import load_model
 import matplotlib.pyplot as plt
 from keras.utils import normalize
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-pickle_in = open("/Users/william/Documents/gitHub/B20IT38/label_dataset/X2.pickle", "rb")
+# FF-NN- <model num> - <regulazation> - <imgsize> - <trained on dataset>
+NAME = "models/base_model/FF-NN-1-reg_l1_l2-64x64-training_d_1"
+
+# Load dataset
+pickle_in = open("/Users/william/Documents/gitHub/B20IT38/label_dataset/X.pickle", "rb")
 X = pickle.load(pickle_in)
 
-pickle_in = open("/Users/william/Documents/gitHub/B20IT38/label_dataset/y2.pickle", "rb")
+pickle_in = open("/Users/william/Documents/gitHub/B20IT38/label_dataset/y.pickle", "rb")
 y = pickle.load(pickle_in)
 
+# Normalize data
 X = normalize(X, axis=1)
 y_label = np.asarray(y)
 
-model = load_model("/Users/william/Documents/gitHub/B20IT38/plaidml_test_model/models/FF-NN-1-test.model")
 
-model.compile(optimizer='adam',  # Good default optimizer to start with
-              loss='sparse_categorical_crossentropy', # how will we calculate our "error." Neural network aims to minimize loss.
-              metrics=['accuracy'])  # what to track
+model = load_model("/Users/william/Documents/gitHub/B20IT38/plaidml_test_model/models/FF-NN-1-test.model")  # Load model
+checkpointer = ModelCheckpoint(filepath='models/base_model/weight_saves/weight_save.hdf5', verbose=1, save_best_only=True)  # saves the models weights after each epoch if the validation loss decreased
+early_stop = EarlyStopping(monitor="val_loss", min_delta=0, patience=9, verbose=0, mode="auto", baseline=None, restore_best_weights=False)  # Stops the training early of the val_loss has stopped improving
 
-history = model.fit(X, y_label, epochs=50, validation_split=0.3) # train the model
+history = model.fit(X, y_label, epochs=10, validation_split=0.3, callbacks=[checkpointer, early_stop])  # train the model
 
 # Plot training & validation accuracy values
 plt.plot(history.history['acc'])
@@ -42,7 +47,6 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
-
 
 model.summary()
 model.save('models/retrained/FF-NN-1-test-1.model')
--- plaidml_model_test.py
+++ plaidml_model_test.py
@@ -8,32 +8,39 @@ os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.utils import normalize
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-NAME = "models/FF-NN-1-test"
+NAME = "models/base_model/FF-NN-1-64x64-training_d_1"  # Model name
 
-pickle_in = open("/Users/william/Documents/gitHub/B20IT38/label_dataset/X.pickle", "rb")
+# Loading dataset
+pickle_in = open("/Users/william/Documents/gitHub/B20IT38/data_and_test_sets/processed/d_1/X.pickle", "rb")
 X = pickle.load(pickle_in)
 
-pickle_in = open("/Users/william/Documents/gitHub/B20IT38/label_dataset/y.pickle", "rb")
+pickle_in = open("/Users/william/Documents/gitHub/B20IT38/data_and_test_sets/processed/d_1/y.pickle", "rb")
 y = pickle.load(pickle_in)
 
 # X = X/255.0
+# Normalizing data
 X = normalize(X, axis=1)
 y_label = np.asarray(y)
 
-
+# Model definition
 model = Sequential()  # a basic feed-forward model
 model.add(Flatten())  # takes our 100x100 and makes it 1x10000
-model.add(Dense(128, activation='relu', input_shape= X.shape[1:]))
-model.add(Dense(128, activation='relu'))  # a simple fully-connected layer, 128 units, relu activation
-model.add(Dense(128, activation='relu'))  # a simple fully-connected layer, 128 units, relu activation
-model.add(Dense(5, activation='softmax'))  # our output layer. 5 units for 5 classes. Softmax for probability distribution
+model.add(Dense(512, activation='relu', input_shape=X.shape[1:], name="dense_1"))
+model.add(Dense(512, activation='relu', name="dense_2"))  # a simple fully-connected layer, 128 units, relu activation
+model.add(Dense(512, activation='relu', name="dense_3"))  # a simple fully-connected layer, 128 units, relu activation
+model.add(Dense(5, activation='softmax', name="dense_output"))  # our output layer. 5 units for 5 classes. Softmax for probability distribution
+
+# model.load_weights("/Users/william/Documents/gitHub/B20IT38/plaidml_test_model/models/base_model/weight_saves/weight_save.hdf5", by_name=True)
 
 model.compile(optimizer='adam',  # Good default optimizer to start with
               loss='sparse_categorical_crossentropy',  # how will we calculate our "error." Neural network aims to minimize loss.
               metrics=['accuracy'])  # what to track
 
-history = model.fit(X, y_label, batch_size=32, epochs=50, validation_split=0.3)
+checkpointer = ModelCheckpoint(filepath='models/base_model/weight_saves/weight_save2.hdf5', verbose=1, save_best_only=True)  # saves the models weights after each epoch if the validation loss decreased
+early_stop = EarlyStopping(monitor="val_loss", min_delta=0, patience=15, verbose=0, mode="auto", baseline=None, restore_best_weights=False)  # Stops the training early of the val_loss has stopped improving
+history = model.fit(X, y_label, batch_size=32, epochs=100, validation_split=0.3, callbacks=[checkpointer, early_stop])  # Trains the model and saves the history of the training
 
 # Plot training & validation accuracy values
 plt.plot(history.history['acc'])
